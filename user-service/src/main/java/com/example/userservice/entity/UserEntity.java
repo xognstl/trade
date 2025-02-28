cabd1d4b39b009e93efd435ef92e11f60b834516
xognstl <xognstl@naver.com>
2025-02-28 16:45:05 +0900

    20250228 commit

--- user-service/src/main/java/com/example/userservice/entity/UserEntity.java
+++ user-service/src/main/java/com/example/userservice/entity/UserEntity.java
@@ -1,4 +1,22 @@
 package com.example.userservice.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Table(name = "users")
+@Getter
+@Entity
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserEntity {
+    @Id
+    private long id;
+    private String username;
+    private String password;
+    private String email;
 }
