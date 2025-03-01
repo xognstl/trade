1a2ecb131ddfb51881f6fbdd5807881d079a3490
xognstl <xognstl@naver.com>
2025-03-01 23:39:14 +0900

    20250301 commit

--- user-service/src/main/java/com/example/userservice/entity/UserEntity.java
+++ user-service/src/main/java/com/example/userservice/entity/UserEntity.java
@@ -1,8 +1,6 @@
 package com.example.userservice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
